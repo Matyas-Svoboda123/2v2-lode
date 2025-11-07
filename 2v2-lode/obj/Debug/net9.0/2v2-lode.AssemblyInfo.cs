723d6057e9f564c73b5f78def1a75bdfccdb2fc4
mates <mates@MATES>
2025-11-07 11:30:26 +0100

    kk

--- 2v2-lode/obj/Debug/net9.0/2v2-lode.AssemblyInfo.cs
+++ 2v2-lode/obj/Debug/net9.0/2v2-lode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2v2-lode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00717f967c496868d796e0e91084b9fb315a3247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3c78d16a80eef2a597c9858880f30c37390aa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("2v2-lode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2v2-lode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
