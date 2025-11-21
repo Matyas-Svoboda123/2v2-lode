aaf628c7ab5e31beca66c016d3aeb151710dd6db
mates <mates@MATES>
2025-11-21 12:06:04 +0100

    strileni na lode

--- 2v2-lode/HraciPole.cs
+++ 2v2-lode/HraciPole.cs
@@ -4,228 +4,233 @@ using System.Runtime.CompilerServices;
 
 namespace _2v2_lode;
 
-public class HraciPole
-{
-    public char[,] pole = new char[10, 10];
-    public int ZbyleLode = 20; 
-
-    public void VypisPole()
+    public class HraciPole
     {
-        for (int i = 0; i < pole.GetLength(0); i++)
+        public char[,] pole = new char[10, 10];
+        public int ZbyleLode = 20;
+
+        public void VypisPole(bool skryte = true)
         {
-            for (int j = 0; j < pole.GetLength(1); j++)
+            for (int i = 0; i < pole.GetLength(0); i++)
             {
-                switch (pole[i, j])
+                for (int j = 0; j < pole.GetLength(1); j++)
                 {
-                    case 'v':
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                        break;
-                    case 'p':
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        break;
-                    case 'm':
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.BackgroundColor = ConsoleColor.White;
-                        break;
-                    case 'l':
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.BackgroundColor = ConsoleColor.Gray;
-                        break;
+                    char c = pole[i, j];
+                    char zobraz = c;
+
+                    if (skryte)
+                    {
+                        // Nepřítel nesmí vidět lodě
+                        if (c == 'l')
+                            zobraz = 'v';  // nebo '?'
+                    }
+
+                    switch (zobraz)
+                    {
+                        case 'v':
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            break;
+                        case 'p':
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            break;
+                        case 'm':
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.BackgroundColor = ConsoleColor.White;
+                            break;
+                    }
+
+                    Console.Write(zobraz + " ");
+                    Console.ResetColor();
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void NaplnPole()
+        {
+            // Naplní celé pole vodou
+            for (int i = 0; i < pole.GetLength(0); i++)
+            {
+                for (int j = 0; j < pole.GetLength(1); j++)
+                {
+                    pole[i, j] = 'v'; // voda
+                }
+            }
+
+            int l1 = 4; // lodě o velikosti 1
+            int l2 = 3; // lodě o velikosti 2
+            int l3 = 2; // lodě o velikosti 3
+            int l4 = 1; // lodě o velikosti 4
+
+            string lod;
+            int x, y;
+            char smer; // h = horizontálně, v = vertikálně
+
+            while (l1 > 0 || l2 > 0 || l3 > 0 || l4 > 0)
+            {
+                Console.Clear();
+                VypisPole();
+
+                Console.WriteLine("\nJakou loď chceš položit? (1–4)");
+                Console.WriteLine($"Loď(1): {l1}, Loď(2): {l2}, Loď(3): {l3}, Loď(4): {l4}");
+
+                // --- výběr platné lodě ---
+                do
+                {
+                    lod = Console.ReadLine();
+                } while (lod != "1" && lod != "2" && lod != "3" && lod != "4");
+
+                int velikost = int.Parse(lod);
+
+                // Zkontroluj, jestli má hráč tuto loď ještě k dispozici
+                if ((velikost == 1 && l1 == 0) ||
+                    (velikost == 2 && l2 == 0) ||
+                    (velikost == 3 && l3 == 0) ||
+                    (velikost == 4 && l4 == 0))
+                {
+                    Console.WriteLine("Tuto loď už nemáš! Zkus jinou.");
+                    Console.ReadKey();
+                    continue;
                 }
 
-                Console.Write(pole[i, j] + ' ');
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.Write("  ");
-            }
+                // --- Zadej pozici a směr ---
+                Console.Write("Zadej X souřadnici (0 - " + (pole.GetLength(0) - 1) + "): ");
+                x = int.Parse(Console.ReadLine());
 
-            Console.WriteLine();
-        }
-    }
+                Console.Write("Zadej Y souřadnici (0 - " + (pole.GetLength(1) - 1) + "): ");
+                y = int.Parse(Console.ReadLine());
 
-    public void NaplnPole()
-    {
-        // Naplní celé pole vodou
-        for (int i = 0; i < pole.GetLength(0); i++)
-        {
-            for (int j = 0; j < pole.GetLength(1); j++)
-            {
-                pole[i, j] = 'v'; // voda
-            }
-        }
+                if (velikost > 1)
+                {
+                    Console.Write("Zadej směr (h = horizontálně, v = vertikálně): ");
+                    smer = char.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    smer = 'h'; // pro velikost 1 směr neřešíme
+                }
 
-        int l1 = 4; // lodě o velikosti 1
-        int l2 = 3; // lodě o velikosti 2
-        int l3 = 2; // lodě o velikosti 3
-        int l4 = 1; // lodě o velikosti 4
+                // --- Ověření, zda může být loď položena ---
+                if (!MuzePolozit(x, y, velikost, smer))
+                {
+                    Console.WriteLine("Nelze umístit loď – mimo pole nebo koliduje s jinou lodí!");
+                    Console.ReadKey();
+                    continue;
+                }
 
-        string lod;
-        int x, y;
-        char smer; // h = horizontálně, v = vertikálně
+                // --- Umístění lodi ---
+                PolozeniLodi(x, y, velikost, smer);
+                Console.WriteLine("Loď položena!");
 
-        while (l1 > 0 || l2 > 0 || l3 > 0 || l4 > 0)
-        {
-            Console.Clear();
-            VypisPole();
+                // --- Snížení počtu dostupných lodí ---
+                switch (velikost)
+                {
+                    case 1: l1--; break;
+                    case 2: l2--; break;
+                    case 3: l3--; break;
+                    case 4: l4--; break;
+                }
 
-            Console.WriteLine("\nJakou loď chceš položit? (1–4)");
-            Console.WriteLine($"Loď(1): {l1}, Loď(2): {l2}, Loď(3): {l3}, Loď(4): {l4}");
-
-            // --- výběr platné lodě ---
-            do
-            {
-                lod = Console.ReadLine();
-            } while (lod != "1" && lod != "2" && lod != "3" && lod != "4");
-
-            int velikost = int.Parse(lod);
-
-            // Zkontroluj, jestli má hráč tuto loď ještě k dispozici
-            if ((velikost == 1 && l1 == 0) ||
-                (velikost == 2 && l2 == 0) ||
-                (velikost == 3 && l3 == 0) ||
-                (velikost == 4 && l4 == 0))
-            {
-                Console.WriteLine("Tuto loď už nemáš! Zkus jinou.");
                 Console.ReadKey();
-                continue;
             }
 
-            // --- Zadej pozici a směr ---
-            Console.Write("Zadej X souřadnici (0 - " + (pole.GetLength(0) - 1) + "): ");
-            x = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Všechny lodě byly úspěšně položeny!");
+            VypisPole();
+        }
 
-            Console.Write("Zadej Y souřadnici (0 - " + (pole.GetLength(1) - 1) + "): ");
-            y = int.Parse(Console.ReadLine());
-
-            if (velikost > 1)
+        private bool MuzePolozit(int x, int y, int velikost, char smer)
+        {
+            if (smer == 'v')
             {
-                Console.Write("Zadej směr (h = horizontálně, v = vertikálně): ");
-                smer = char.Parse(Console.ReadLine());
+                if (x + velikost > pole.GetLength(0)) return false;
+                for (int i = 0; i < velikost; i++)
+                {
+                    if (pole[x + i, y] != 'v') return false;
+                }
+            }
+            else // vertikálně
+            {
+                if (y + velikost > pole.GetLength(1)) return false;
+                for (int i = 0; i < velikost; i++)
+                {
+                    if (pole[x, y + i] != 'v') return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void PolozeniLodi(int x, int y, int velikost, char smer)
+        {
+            if (smer == 'v')
+            {
+                for (int i = 0; i < velikost; i++)
+                {
+                    pole[x + i, y] = 'l';
+                }
             }
             else
             {
-                smer = 'h'; // pro velikost 1 směr neřešíme
+                for (int i = 0; i < velikost; i++)
+                {
+                    pole[x, y + i] = 'l';
+                }
             }
+        }
 
-            // --- Ověření, zda může být loď položena ---
-            if (!MuzePolozit(x, y, velikost, smer))
+
+        public bool Strilej()
+        {
+            // Upozornění: int.Parse bez try/catch může způsobit pád, pokud uživatel zadá text
+            Console.Write("Zadej X souřadnici (0 - " + (pole.GetLength(0) - 1) + "): ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Zadej Y souřadnici (0 - " + (pole.GetLength(1) - 1) + "): ");
+            int y = int.Parse(Console.ReadLine());
+
+            // Kontrola mezí (bez return false v if)
+            bool vMezich = true;
+            if (x < 0) vMezich = false;
+            if (x >= pole.GetLength(0)) vMezich = false;
+            if (y < 0) vMezich = false;
+            if (y >= pole.GetLength(1)) vMezich = false;
+
+
+            if (!vMezich)
             {
-                Console.WriteLine("Nelze umístit loď – mimo pole nebo koliduje s jinou lodí!");
+                Console.WriteLine("\nStřela je mimo hrací pole! Zkus to znovu.");
                 Console.ReadKey();
-                continue;
+                return false; // Nepočítá se jako platný tah
             }
 
-            // --- Umístění lodi ---
-            PolozeniLodi(x, y, velikost, smer);
-            Console.WriteLine("Loď položena!");
-
-            // --- Snížení počtu dostupných lodí ---
-            switch (velikost)
+            char cil = pole[x, y];
+            if (cil == 'v') // Voda - Miss
             {
-                case 1: l1--; break;
-                case 2: l2--; break;
-                case 3: l3--; break;
-                case 4: l4--; break;
+                pole[x, y] = 'm';
+                Console.WriteLine("\n[VEDLE] Střela minula cíl (Voda).");
+            }
+            else if (cil == 'l') // Loď - Hit
+            {
+                pole[x, y] = 'p';
+                ZbyleLode--; // Upraveno: Používá ZbyleLode přímo místo this.ZbyleLode
+                Console.WriteLine("\n[ZÁSAH] Trefil jsi loď!");
+            }
+            else // Už trefeno nebo minuto
+            {
+                Console.WriteLine("\n[UPOZORNĚNÍ] Už jsi střílel na toto místo. Zkus to znovu.");
+                Console.ReadKey();
+                return false; // Nepočítá se jako platný tah
             }
 
+            Console.WriteLine("\nStav pole po střele:");
+            VypisPole();
+
+            Console.WriteLine("\nStiskni libovolnou klávesu pro pokračování...");
             Console.ReadKey();
-        }
-
-        Console.Clear();
-        Console.WriteLine("Všechny lodě byly úspěšně položeny!");
-        VypisPole();
-    }
-
-    private bool MuzePolozit(int x, int y, int velikost, char smer)
-    {
-        if (smer == 'h')
-        {
-            if (x + velikost > pole.GetLength(0)) return false;
-            for (int i = 0; i < velikost; i++)
-            {
-                if (pole[x + i, y] != 'v') return false;
-            }
-        }
-        else // vertikálně
-        {
-            if (y + velikost > pole.GetLength(1)) return false;
-            for (int i = 0; i < velikost; i++)
-            {
-                if (pole[x, y + i] != 'v') return false;
-            }
-        }
-
-        return true;
-    }
-
-    private void PolozeniLodi(int x, int y, int velikost, char smer)
-    {
-        if (smer == 'h')
-        {
-            for (int i = 0; i < velikost; i++)
-            {
-                pole[x + i, y] = 'l';
-            }
-        }
-        else
-        {
-            for (int i = 0; i < velikost; i++)
-            {
-                pole[x, y + i] = 'l';
-            }
+            return true; // Platný tah
         }
     }
-    
-    public bool Strilej()
-    {
-        // Upozornění: int.Parse bez try/catch může způsobit pád, pokud uživatel zadá text
-        Console.Write("Zadej X souřadnici (0 - " + (pole.GetLength(0) - 1) + "): ");
-        int x = int.Parse(Console.ReadLine());
-
-        Console.Write("Zadej Y souřadnici (0 - " + (pole.GetLength(1) - 1) + "): ");
-        int y = int.Parse(Console.ReadLine());
-        
-        // Kontrola mezí (bez return false v if)
-        bool vMezich = true;
-        if (x < 0) vMezich = false;
-        if (x >= pole.GetLength(0)) vMezich = false;
-        if (y < 0) vMezich = false;
-        if (y >= pole.GetLength(1)) vMezich = false;
-
-
-        if (!vMezich)
-        {
-            Console.WriteLine("\nStřela je mimo hrací pole! Zkus to znovu.");
-            Console.ReadKey();
-            return false; // Nepočítá se jako platný tah
-        }
-        
-        char cil = pole[x, y];
-        if (cil == 'v') // Voda - Miss
-        {
-            pole[x, y] = 'm';
-            Console.WriteLine("\n[VEDLE] Střela minula cíl (Voda).");
-        }
-        else if (cil == 'l') // Loď - Hit
-        {
-            pole[x, y] = 'p';
-            ZbyleLode--; // Upraveno: Používá ZbyleLode přímo místo this.ZbyleLode
-            Console.WriteLine("\n[ZÁSAH] Trefil jsi loď!");
-        }
-        else // Už trefeno nebo minuto
-        {
-            Console.WriteLine("\n[UPOZORNĚNÍ] Už jsi střílel na toto místo. Zkus to znovu.");
-            Console.ReadKey();
-            return false; // Nepočítá se jako platný tah
-        }
-
-        Console.WriteLine("\nStav pole po střele:");
-        VypisPole(); 
-
-        Console.WriteLine("\nStiskni libovolnou klávesu pro pokračování...");
-        Console.ReadKey();
-        return true; // Platný tah
-    }
-}
--- 2v2-lode/Program.cs
+++ 2v2-lode/Program.cs
@@ -41,42 +41,140 @@
         }
 
         static void NovaHra()
+{
+    Console.Clear();
+    Console.WriteLine("=== NOVÁ HRA 2v2 ===");
+    Console.WriteLine("Zadejte jména hráčů:");
+
+    // --- Tým 1 ---
+    Console.Write("Tým 1 - Hráč 1: ");
+    string t1p1 = Console.ReadLine();
+    Console.WriteLine("Tady je tvé hrací pole, naplň si ho");
+    Console.ReadKey();
+    HraciPole t1p1_pole = new HraciPole();
+    t1p1_pole.NaplnPole();
+
+    Console.Write("Tým 1 - Hráč 2: ");
+    string t1p2 = Console.ReadLine();
+    Console.WriteLine("Tady je tvé hrací pole, naplň si ho");
+    Console.ReadKey();
+    HraciPole t1p2_pole = new HraciPole();
+    t1p2_pole.NaplnPole();
+
+    // --- Tým 2 ---
+    Console.Write("Tým 2 - Hráč 1: ");
+    string t2p1 = Console.ReadLine();
+    Console.WriteLine("Tady je tvé hrací pole, naplň si ho");
+    Console.ReadKey();
+    HraciPole t2p1_pole = new HraciPole();
+    t2p1_pole.NaplnPole();
+
+    Console.Write("Tým 2 - Hráč 2: ");
+    string t2p2 = Console.ReadLine();
+    Console.WriteLine("Tady je tvé hrací pole, naplň si ho");
+    Console.ReadKey();
+    HraciPole t2p2_pole = new HraciPole();
+    t2p2_pole.NaplnPole();
+
+
+    Console.Clear();
+    Console.WriteLine("\nTýmy připraveny!");
+    Console.WriteLine($"{t1p1} a {t1p2}  VS  {t2p1} a {t2p2}");
+    Console.WriteLine("\nStiskni libovolnou klávesu pro start hry...");
+    Console.ReadKey();
+
+    // ====== HERNÍ SMYČKA ======
+    int tah = 0;
+
+    while (true)
+    {
+        Console.Clear();
+        string aktivniJmeno = "";
+        HraciPole aktivniPole = null;
+
+        // určení hráče podle tahu
+        switch (tah % 4)
         {
-            Console.Clear();
-            Console.WriteLine("=== NOVÁ HRA 2v2 ===");
-            Console.WriteLine("Zadejte jména hráčů:");
-                Console.Write("Tým  - Hráč 1: ");
-                string t1p1 = Console.ReadLine();
-                Console.WriteLine("Tady je tvé hrací pole naplň si ho");
-                Console.ReadKey();
-                HraciPole h1 = new HraciPole();
-                h1.NaplnPole();
-                Console.Write("Tým 1 - Hráč 2: ");
-                string t1p2 = Console.ReadLine();
-                Console.WriteLine("Tady je tvé hrací pole naplň si ho");
-                Console.ReadKey();
-                HraciPole h2 = new HraciPole();
-                h1.NaplnPole();
-
-                Console.Write("Tým 2 - Hráč 1: ");
-                string t2p1 = Console.ReadLine();
-                Console.WriteLine("Tady je tvé hrací pole naplň si ho");
-                Console.ReadKey();
-                HraciPole h3 = new HraciPole();
-                h1.NaplnPole();
-
-                Console.Write("Tým 2 - Hráč 2: ");
-                string t2p2 = Console.ReadLine();
-                Console.WriteLine("Tady je tvé hrací pole naplň si ho");
-                Console.ReadKey();
-                HraciPole h4 = new HraciPole();
-                h1.NaplnPole();
-            
-            Console.WriteLine($"\nTýmy připraveny!");
-            Console.WriteLine($"{t1p1} a {t1p2}  VS  {t2p1} a {t2p2}");
-            Console.WriteLine("\nStiskni libovolnou klávesu pro návrat do menu...");
-            Console.ReadKey();
+            case 0:
+                aktivniJmeno = t1p1;
+                aktivniPole = t1p1_pole;
+                break;
+            case 1:
+                aktivniJmeno = t2p1;
+                aktivniPole = t2p1_pole;
+                break;
+            case 2:
+                aktivniJmeno = t1p2;
+                aktivniPole = t1p2_pole;
+                break;
+            case 3:
+                aktivniJmeno = t2p2;
+                aktivniPole = t2p2_pole;
+                break;
         }
+
+        Console.WriteLine($"=== Na tahu je hráč: {aktivniJmeno} ===");
+
+        // Výběr cíle
+        HraciPole cil = null;
+        string cilJmeno = "";
+
+        if (aktivniJmeno == t1p1 || aktivniJmeno == t1p2)
+        {
+            Console.WriteLine("Vyber hráče z Týmu 2, na kterého chceš střílet:");
+            Console.WriteLine("1) " + t2p1 + (t2p1_pole.ZbyleLode == 0 ? " (mrtvý)" : ""));
+            Console.WriteLine("2) " + t2p2 + (t2p2_pole.ZbyleLode == 0 ? " (mrtvý)" : ""));
+
+            int volba = 0;
+            while (volba != 1 && volba != 2)
+                int.TryParse(Console.ReadLine(), out volba);
+
+            if (volba == 1) { cil = t2p1_pole; cilJmeno = t2p1; }
+            else { cil = t2p2_pole; cilJmeno = t2p2; }
+        }
+        else
+        {
+            Console.WriteLine("Vyber hráče z Týmu 1, na kterého chceš střílet:");
+            Console.WriteLine("1) " + t1p1 + (t1p1_pole.ZbyleLode == 0 ? " (mrtvý)" : ""));
+            Console.WriteLine("2) " + t1p2 + (t1p2_pole.ZbyleLode == 0 ? " (mrtvý)" : ""));
+
+            int volba = 0;
+            while (volba != 1 && volba != 2)
+                int.TryParse(Console.ReadLine(), out volba);
+
+            if (volba == 1) { cil = t1p1_pole; cilJmeno = t1p1; }
+            else { cil = t1p2_pole; cilJmeno = t1p2; }
+        }
+
+        if (cil.ZbyleLode == 0)
+        {
+            Console.WriteLine("Tento hráč už nemá lodě! Vyber někoho jiného.");
+            Console.ReadKey();
+            continue;
+        }
+
+        Console.WriteLine($"\n{aktivniJmeno} střílí na: {cilJmeno}");
+        bool platnyTah = cil.Strilej();
+
+        // Zkontroluj, zda některý tým neprohrál
+        if (t1p1_pole.ZbyleLode == 0 && t1p2_pole.ZbyleLode == 0)
+        {
+            Console.WriteLine("\n=== Tým 2 VYHRÁVÁ! ===");
+            Console.ReadKey();
+            return;
+        }
+        if (t2p1_pole.ZbyleLode == 0 && t2p2_pole.ZbyleLode == 0)
+        {
+            Console.WriteLine("\n=== Tým 1 VYHRÁVÁ! ===");
+            Console.ReadKey();
+            return;
+        }
+
+        if (platnyTah)
+            tah++; // pouze platný tah se počítá
+    }
+}
+
 
         static void Pravidla()
         {
